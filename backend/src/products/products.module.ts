a30cff74317e5bd2fb649f6d557dcc6cb4a12053
xXCHRISXx3571 <christiandavidruizmedia@gmail.com>
2026-08-09 18:48:01 -0500

    fix: importar AuthModule

--- backend/src/products/products.module.ts
+++ backend/src/products/products.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from '../auth/auth.module';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 import { Product, ProductSchema } from './products.schema';
 
 @Module({
   imports: [
+    AuthModule,
     MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
   ],
   controllers: [ProductsController],
